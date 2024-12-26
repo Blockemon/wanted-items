197eca0892d293bf1f3ea37a356a93910e18f594
fractalbarista <fractalbarista@gmail.com>
2024-12-26 22:05:15 +0900

    Added feature playing SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP when successfully gave random Pokemon

--- src/main/java/kiwiapollo/wanteditems/luckybox/PokemonLuckyBox.java
+++ src/main/java/kiwiapollo/wanteditems/luckybox/PokemonLuckyBox.java
@@ -7,6 +7,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -32,6 +34,7 @@ public class PokemonLuckyBox extends Item {
             user.getStackInHand(hand).decrement(1);
         }
 
+        user.playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.PLAYERS, 1F, 1F);
         return TypedActionResult.success(user.getStackInHand(hand));
     }
 }
