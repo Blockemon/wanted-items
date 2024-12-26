a57b8937b6a7a19e91da8aa095f3d2594eb3ce01
fractalbarista <fractalbarista@gmail.com>
2024-12-26 16:24:20 +0900

    Removed CaughtBallSwapper.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,31 +1,15 @@
 package com.example.mixin;
 
-import com.cobblemon.mod.common.item.PokeBallItem;
-import com.example.swapper.PropertySwapperItems;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PokeBallItem.class)
+@Mixin(MinecraftServer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "use", cancellable = true)
-	private void cancelOnUsingCaughtBallSwapper(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> info) {
-		if (user.getStackInHand(getOtherHand(hand)).getItem().equals(PropertySwapperItems.CAUGHT_BALL_SWAPPER.getItem())) {
-			info.setReturnValue(TypedActionResult.pass(user.getStackInHand(hand)));
-			info.cancel();
-		}
-	}
-
-	private Hand getOtherHand(Hand hand) {
-		return switch (hand) {
-			case MAIN_HAND -> Hand.OFF_HAND;
-			case OFF_HAND -> Hand.MAIN_HAND;
-		};
+	@Inject(at = @At("HEAD"), method = "loadWorld")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadWorld()V
 	}
 }
--- src/main/java/com/example/swapper/PropertySwapperItems.java
+++ src/main/java/com/example/swapper/PropertySwapperItems.java
@@ -5,7 +5,6 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 
 public enum PropertySwapperItems {
-    CAUGHT_BALL_SWAPPER("caught_ball_swapper", new CaughtBallSwapper()),
     GENDER_SWAPPER("gender_swapper", new GenderSwapper()),
     SHINY_SWAPPER("shiny_swapper", new ShinySwapper());
 
