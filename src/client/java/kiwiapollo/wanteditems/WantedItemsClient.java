5c00420810d7680c2f4a657934f4b31153d95e85
fractalbarista <fractalbarista@gmail.com>
2024-12-26 18:58:21 +0900

    Renamed to WantedItemsClient

--- src/client/java/kiwiapollo/wanteditems/WantedItemsClient.java
+++ src/client/java/kiwiapollo/wanteditems/WantedItemsClient.java
@@ -2,7 +2,7 @@ package kiwiapollo.wanteditems;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class TemplateModClient implements ClientModInitializer {
+public class WantedItemsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
