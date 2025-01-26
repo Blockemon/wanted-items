99ae5a0933df39304b80f40b09f6adb1d97cc055
fractalbarista <fractalbarista@gmail.com>
2025-01-27 00:17:39 +0900

    Edited to print error message in red

--- src/main/java/kiwiapollo/wanteditems/stateditor/GoldBottleCap.java
+++ src/main/java/kiwiapollo/wanteditems/stateditor/GoldBottleCap.java
@@ -15,6 +15,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -51,7 +52,7 @@ public class GoldBottleCap extends Item implements PokemonSelectingItem {
     public @Nullable TypedActionResult<ItemStack> applyToPokemon(@NotNull ServerPlayerEntity player, @NotNull ItemStack itemStack, @NotNull Pokemon pokemon) {
         if (isPerfectIVs(pokemon)) {
             player.playSound(SoundEvents.ITEM_SHIELD_BLOCK, SoundCategory.PLAYERS, 1F, 1F);
-            player.sendMessage(Text.translatable("item.wanteditems.error.has_maximum_stats", pokemon.getSpecies().getTranslatedName()));
+            player.sendMessage(Text.translatable("item.wanteditems.error.has_maximum_stats", pokemon.getSpecies().getTranslatedName()).formatted(Formatting.RED));
             return TypedActionResult.pass(itemStack);
         }
 
